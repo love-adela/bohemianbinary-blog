a12871d73824af576a5e9aaa915dfeb16f0c4c57
love-adelar <love.adelar@gmail.com>
2018-06-27 14:15:36 +0900

    Declared a Mapping

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
 engine = create_engine('sqlite:///history.db')
-base = declarative_base()
+Base = declarative_base()
 
 
 class History(Base):
@@ -17,3 +17,8 @@ class History(Base):
     high_price = Column(float)
     low_price = Column(float)
     close_price = Column(float)
+
+
+def __repr__(self):
+    return "<User(open_price ='%f', hight_price='%f', low_price='%f', close_price='%f')>" % (
+        self.date, self.open_price, self.high_price, self.low_price, self.close_price)
