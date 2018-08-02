3b725bab5191bc8848d5a68741ff8bcbc242787b
love-adelar <love.adelar@gmail.com>
2018-08-02 22:25:39 +0900

    Ranked results by timepass

--- temp.py
+++ temp.py
@@ -1,20 +1,16 @@
 import db
-import requests
-import bs4
-import pandas
-import datetime
 
 class CoinRepository:
     def __init__(self, coin_type):
         self.coin_type = coin_type
         self.coin_full_name = {
             'ETH': 'ethereum',
-            'BTC': 'bitcoin',
+            'BTC':'bitcoin',
             'XRP': 'ripple',
-            'BCH': 'bitcoin_cash',
+            'BCH': 'bitcoin-cash',
             'EOS': 'eos',
             'XLM': 'stellar',
-            'LTC': 'lite_coin',
+            'LTC': 'litecoin',
             'ADA': 'cardano',
             'MIOTA': 'iota',
             'USDT': 'tether'
@@ -25,3 +21,7 @@ class CoinRepository:
         db.create_db()
 
     def print_stock(self):
+        session = db.Session()
+        for row in session.query(db.History).filter_by(db.History.coin_type == self.coin_type)\
+            .order_by(db.asc(db.History.date).all()):
+
