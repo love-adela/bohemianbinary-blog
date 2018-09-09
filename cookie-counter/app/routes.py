1850873f9ab2dca94bf1ca5d42791f13c1ff3a69
love-adelar <love.adelar@gmail.com>
2018-09-09 13:07:47 +0900

    rendered template

--- cookie-counter/app/routes.py
+++ cookie-counter/app/routes.py
@@ -1,22 +1,26 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
 
+
 @app.route("/admin-anl/director")
 def admin_anl():
-    movie = {'movie_name':'Searching'}
-    director = {'director_name' : 'Aneesh Chaganty'}
-    return '''
-<html>
-    <head>
-        <title>Director List</title>
-    </head>
-    <body>
-        <h1>Director of ''' + movie['name'] + ''' is ''' + director['name'] + '''</h1>
-    </body>
-</html>
-    '''
+    movie = [
+        {'movie_name':'Searching'},
+        {'movie_name': 'The Predator'},
+        {'movie_name': 'Mission : Impossible - Fallout'}
+
+    ]
+    director = [
+        {'director_name' : 'Aneesh Chaganty'},
+        {'director_name': 'Shane Black'},
+        {'director_name': 'Christopher McQuarrie'},
+
+    ]
+    return render_template('admin-anl.html', title='Movie Director', movie=movie, director=director)
 
