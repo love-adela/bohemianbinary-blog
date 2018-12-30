e63805fe6e5cc3c6c936fefdc997d11a5feb5e06
love-adelar <love.adelar@gmail.com>
2018-12-30 23:48:06 +0900

    DB 모델 변경

--- cookie-counter/app/models.py
+++ cookie-counter/app/models.py
@@ -14,17 +14,17 @@ def create_db():
     Base.metadata.create_all(engine)
 
 
-movie_with_director = db.Table('movie_with_director',
-    db.Column('movie_id', Integer, ForeignKey('movie.id')),
-    db.Column('director_id', Integer, ForeignKey('director.id'))
-)
+movie_with_director = Table('movie_with_director',
+                            db.Model.metadata,
+                            Column('movie_id', Integer, ForeignKey('movie.id')),
+                            Column('director_id', Integer, ForeignKey('director.id'))
+                            )
 
-actor_association_table = Table(
-    'actor_association',
-    db.Model.metadata,
-    Column('movie_id', Integer, ForeignKey('movie.id')),
-    Column('actor_id', Integer, ForeignKey('actor.id'))
-)
+actor_association_table = Table('actor_association',
+                                db.Model.metadata,
+                                Column('movie_id', Integer, ForeignKey('movie.id')),
+                                Column('actor_id', Integer, ForeignKey('actor.id'))
+                                )
 
 
 class Movie(db.Model):
@@ -33,13 +33,29 @@ class Movie(db.Model):
     name_kr = Column(String(120))
     photo = Column(String(120))
     directors = relationship("Director",
+                             # primaryjoin=movie_with_director,
                              secondary=movie_with_director,
-                             backref="movies")
-    actors = relationship("Actor",
-                          secondary=actor_association_table,
-                          backref="movies")
-    cookies = relationship("Cookie", backref="movie")
+                             backref=db.backref("movies", lazy='dynamic'))
     image_file_name = Column(String(120))
+
+    def add(self, movie):
+        if not self.is_movie(movie):
+            self.directors.append(movie)
+
+    def emit(self, movie):
+        if self.is_movie(movie):
+            self.directors.remove(movie)
+
+    def is_movie(self, movie):
+        return self.directors.filter(
+            movie.c.director_id == movie.id).count() > 0
+
+    def directors_lists(self):
+        director = Director.query.join(
+            movie_with_director, (movie_with_director.c.directors_id == Director.movie_id)).filter(
+            movie_with_director.c.movie_id == self.id)
+        own = Director.query.filter_by(user_id=self.id)
+        return director.union(own)
 
     def __repr__(self):
         return "<Movie('%s', ('%s'))>" % (self.name_en, self.name_kr)
@@ -50,6 +66,7 @@ class Director(db.Model):
     name_en = Column(String(120))
     name_kr = Column(String(120))
     photo = Column(String(120))
+    movie_id = Column(Integer, ForeignKey('movie.id'))
 
     def __repr__(self):
         return "<Movie Director('%s', ('%s'))>" % (self.name_en, self.name_kr)
