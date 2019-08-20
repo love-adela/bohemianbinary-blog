592fa5344c956a149fbc7f21367e8de639033f0a
Adela Chung <love-adelar@users.noreply.github.com>
2019-08-20 17:20:45 +0900

    Revert "Test markdown"

--- django-blog/mysite/blog/models.py
+++ django-blog/mysite/blog/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 from django.utils import timezone
-from django.utils.html import mark_safe
 from django.urls import reverse_lazy
-from markdown import markdown
-
 
 try:
     from unicode import unicode
@@ -49,6 +46,3 @@ class Post(models.Model):
     def publish(self):
         self.published_date = timezone.now()
         self.save()
-    
-    def get_text_as_markdown(self):
-        return mark_safe(markdown(self.text, safe_mode='escape'))
