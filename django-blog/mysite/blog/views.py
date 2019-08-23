236169d3c2720380a4e815f6cd212a09c5f4b4fa
love-adelar <love.adelar@gmail.com>
2019-08-23 22:57:15 +0900

    django model을 상속받은 Post 클래스를 연결시켜 posts 에 저장

--- django-blog/mysite/blog/views.py
+++ django-blog/mysite/blog/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from .models import Post, Tag, BaseFormatter, FormatterA
+from .models import Post, Tag
 from .forms import PostForm
 
 
 def post_list(request):
-    posts = BaseFormatter.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts, 'formatter': FormatterA})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
+# markdown_method == 'markdownify':
+    # formatter = FormatterMarkdownify
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
