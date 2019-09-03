bf81d2e3471718448e1d86f95ede0e171a33c79c
love-adelar <love.adelar@gmail.com>
2019-09-04 01:27:25 +0900

    불필요한 주석 삭제

--- django-blog/mysite/blog/views.py
+++ django-blog/mysite/blog/views.py
@@ -8,9 +8,6 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
-# markdown_method == 'markdownify':
-    # formatter = FormatterMarkdownify
-
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
