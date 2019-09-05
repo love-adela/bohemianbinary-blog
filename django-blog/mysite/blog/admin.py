b4f3e0eb894b982016f3fdc71e30a17888d4369b
love-adelar <love.adelar@gmail.com>
2019-09-06 00:14:03 +0900

    Comment 모델 생성 및 애플리케이션에 적용

--- django-blog/mysite/blog/admin.py
+++ django-blog/mysite/blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post, Tag
+from .models import Comment, Post, Tag
 
 # Register your models here.
+admin.site.register(Comment)
 admin.site.register(Post)
 admin.site.register(Tag)
