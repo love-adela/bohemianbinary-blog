82a7279a9a908890ea4adbcb0de85b66991459e6
love-adelar <love.adelar@gmail.com>
2019-09-05 21:28:53 +0900

    Draft 기능 구현

--- django-blog/mysite/blog/views.py
+++ django-blog/mysite/blog/views.py
@@ -18,7 +18,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -33,7 +32,6 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
