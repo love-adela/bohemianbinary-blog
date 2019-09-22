de8b9974e2afe386faeb6e228bb50846a0aae536
love-adelar <love.adelar@gmail.com>
2019-09-22 21:22:43 +0900

    urls : PEP 규격 맞춰 line break함

--- django-blog/mysite/mysite/urls.py
+++ django-blog/mysite/mysite/urls.py
@@ -20,7 +20,8 @@ from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/login', views.LoginView.as_view(), name='login'),
-    path('accounts/logout', views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(next_page='/'),
+         name='logout'),
     path('', include('blog.urls')),
 ]
