11af4b3de214f9165b44632dbea598185e80352f
love-adelar <love.adelar@gmail.com>
2019-10-05 22:17:49 +0900

    Test: IndexView, DetailView 테스트

--- django-blog/mysite/blog/tests.py
+++ django-blog/mysite/blog/tests.py
@@ -1,8 +1,9 @@
 import datetime
 
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.urls import reverse
 from .models import Image, Tag, Post, Comment, upload_to
 
 from .utils import FormatterMistune
@@ -32,28 +33,45 @@ class TagModelTests(TestCase):
         self.assertNotEquals(t1.title, 'python3')
 
 
-class PostModelTests(TestCase):
-    # author 초기화
-    def create_user(self):
-        author = User.objects.create(username='testuser')
-        author.set_password('1234')
-        author.save()
-        return User.objects.first()
+def create_user():
+    """
+    author 초기화
+    """
+    author = User.objects.create(username='testuser')
+    author.set_password('1234')
+    author.save()
+    return User.objects.first()
 
+
+def create_post(title=None, text=None, days=None):
+    """
+    post 초기화
+    """
+    author = create_user()
+    post = Post.objects.create(author=author)
+    if title is not None:
+        post.title = title
+    if text is not None:
+        post.text = text
+    if days is not None:
+        post.created_date = days
+    return post
+    # time = timezone.now() + datetime.timedelta(days=days)
+
+
+class PostModelTests(TestCase):
     # method 테스트
     def test_publish(self):
-        author = self.create_user()
-
-        # post에서 publish되는 시간대가 현재시간보다 뒤에 있는지 테스트
+        """
+        post에서 publish되는 시간대가 현재시간보다 뒤에 있는지 테스트
+        """
         now = timezone.now()
-        post = Post(author=author)  # author이 필수값이라서 설정함.
+        post = create_post()
         post.publish()
         self.assertLess(now, post.published_date)
 
     def test_formatted_text(self):
-        author = self.create_user()
-
-        post = Post.objects.create(author=author)
+        post = create_post()
         post.text = '**Java가 좋아요**'
         # import한 mistune 포매트가 동작하는지 테스트
         # ... 만일 mistune라이브러리가 모든 text를 1로 format해도 정상으로 나오면?
@@ -66,26 +84,55 @@ class PostModelTests(TestCase):
         self.assertEquals(formatted_another_text, post.formatted_text())
 
     def test_approved_comments(self):
-        author = self.create_user()
-        post = Post.objects.create(author=author)
-
+        post = create_post()
         c1 = post.comments.create(author='test', text='와아 축하해요!')
         c1.approve()
         post.save()
         c2 = post.approved_comments().first()
         self.assertEqual(c2.text, '와아 축하해요!')
 
-    # string 포함 테스트
     def test_is_contained(self):
+        """
+        string 포함 테스트
+        """
         Image.objects.create(file='img/pig1.jpg')
         i1 = Image.objects.first()
         self.assertIn(str(i1), '/Users/adela/workspace/bohemian-binary/django-blog/mysite/media/img/pig1.jpg')
         Tag.objects.create(title='java')
         t1 = Tag.objects.first()
         self.assertIn(str(t1), 'java')
-        author = self.create_user()
-        post = Post.objects.create(title='이거슨 테스트', author=author)
+        post = create_post(title='이거슨 테스트')
         p1 = Post.objects.first()
         self.assertIn(str(p1), '이거슨 테스트')
         c1 = post.comments.create(author='test', text='이거슨 댓글테스트')
         self.assertIn(str(c1), '이거슨 댓글테스트')
+
+
+class PostIndexViewTests(TestCase):
+    def test_no_post(self):
+        """
+        If no post exists, an appropriate message is displayed.
+        """
+        client = Client()
+        response = self.client.get(reverse('post_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['posts'], [])
+
+
+class PostDetailViewTests(TestCase):
+    def test_is_post(self):
+        """
+        """
+        post = create_post(title='테스트 포스트 1.')
+        post = Post.objects.filter(uuid=post.uuid).first()
+        client = Client()
+        response = self.client.get(
+            reverse('post_detail', args=(post.uuid,)))
+        self.assertEqual(
+            response.context['post'], post
+        )
+
+
+class PostCreateViewTests(TestCase):
+    def test_is_form_valid(self):
+        post = create_post()
