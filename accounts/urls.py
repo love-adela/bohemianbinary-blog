65dfd5ce6b194e3fbda270d7ce97bfc09c2c9ddd
love-adelar <love.adelar@gmail.com>
2020-01-05 20:53:59 +0900

    URL: 회원가입, 로그인, 로그아웃 기능 넣음

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns= [
-    path('signup', views.AccountCreateView.as_view(), name='signup'),
-    # path('login/', views.login, name='login'),
-    # path('logout/', views.logout, name='logout')
+    path('signup/', views.AccountCreateView.as_view(), name='signup'),
+    path('login/', views.LoginCreateView.as_view(), name='login'),
+    path('logout/', views.LogoutRedirectView.as_view(), name='logout')
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -20,8 +20,6 @@ from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/login/', views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', views.LogoutView.as_view(next_page='/'),
-         name='logout'),
     path('', include('blog.urls')),
+    path('', include('accounts.urls'))
 ]
