d9c2a919907e38ab5ef839e94e9995c41a39725c
love-adelar <love.adelar@gmail.com>
2020-01-11 00:45:49 +0900

    next로 넘겨 받아 redirect_url을 연결해줌

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
@@ -11,14 +12,14 @@ import logging
 
 class AccountCreateView(generic.edit.CreateView):
     def post(self, request):
-        redirect_to = request.GET.get('next', '')
-        if not redirect_to:
-            redirect_to = default.LOGIN_REDIRECT_URL
+        redirect_url = request.GET.get('next', settings.LOGIN_REDIRECT_URL)
+        if not redirect_url:
+            redirect_url = settings.LOGIN_REDIRECT_URL
         signup_form = UserCreationForm(request.POST)
         if signup_form.is_valid():
             user = signup_form.save()
             auth_login(request, user)
-            return redirect('redirect_to') 
+            return redirect(redirect_url) 
         return render(request, 'accounts/signup.html', {'signup_form':signup_form})
 
     def get(self, request):
@@ -28,13 +29,13 @@ class AccountCreateView(generic.edit.CreateView):
 
 class LoginCreateView(generic.edit.CreateView):
     def post(self, request):
-        redirect_to = request.GET.get('next', '')
-        if not redirect_to:
-            redirect_to = default.LOGIN_REDIRECT_URL
+        redirect_url = request.GET.get('next', settings.LOGIN_REDIRECT_URL)
+        if not redirect_url:
+            redirect_url = settings.LOGIN_REDIRECT_URL
         login_form = AuthenticationForm(request, request.POST)
         if login_form.is_valid():
             auth_login(request, login_form.get_user())
-            return redirect('redirect_to')
+            return redirect(redirect_url)
         return render(request, 'accounts/login.html', {'login_form': login_form})
 
     def get(self, request):
