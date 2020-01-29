fbc13e5adbb3f6918ed7b2674600c9c2e2b39fe1
love-adelar <love.adelar@gmail.com>
2020-01-29 23:56:02 +0900

    eol 추가 및 AuthenticationForm로 상속 받아 LoginForm 간단하게 구현

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,17 +1,23 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.forms import EmailField
+
 
 class CreateUserForm(UserCreationForm):
     email = forms.EmailField(required=True)
-    
+
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
-        
+
     def save(self, commit=True):
         user = super(CreateUserForm, self).save(commit=False)
         user.email = self.cleaned_data["email"]
         if commit:
             user.save()
         return user
+
+
+class LoginForm(AuthenticationForm):
+    username = EmailField(widget=forms.EmailInput(attrs={'autofocus': True}))
