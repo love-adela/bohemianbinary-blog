6794a3fa26e9423794b699aa458255ba6a27e6e7
love-adelar <love.adelar@gmail.com>
2020-01-29 23:55:15 +0900

    authentication_form으로 LoginForm 연결. 전에 있던 코드는 삭제했음.

--- accounts/views.py
+++ accounts/views.py
@@ -11,40 +11,9 @@ class AccountCreateView(generic.edit.CreateView):
     form_class = CreateUserForm
     success_url = reverse_lazy('login')
 
-    # def post(self, request):
-    #     redirect_url = request.GET.get('next', settings.LOGIN_REDIRECT_URL)
-    #     signup_form = UserCreationForm(request.POST)
-    #     if signup_form.is_valid():
-    #         user = signup_form.save()
-    #         auth_login(request, user)
-    #         return redirect(redirect_url)
-    #     return render(request, 'accounts/signup.html',
-    #  {'signup_form':signup_form})
-
-    # def get(self, request):
-    #     signup_form = UserCreationForm()
-    #     return render(request, 'accounts/signup.html',
-    #  {'signup_form':signup_form})
-
-
-# class LoginCreateView(generic.edit.CreateView):
-#     success_url = '/'
-
-#     def post(self, request):
-#         redirect_url = request.GET.get('next', settings.LOGIN_REDIRECT_URL)
-#         login_form = AuthenticationForm(request, request.POST)
-#         if login_form.is_valid():
-#             auth_login(request, login_form.get_user())
-#             return redirect(redirect_url)
-#         return render(request, 'accounts/login.html',
-#  {'login_form': login_form})
-
-#     def get(self, request):
-#         login_form = AuthenticationForm()
-#         return render(request, 'accounts/login.html',
-#  {'login_form': login_form})
 
 class UserLoginView(LoginView):
+    authentication_form = LoginForm
     template_name = 'accounts/login.html'
 
     def form_invalid(self, form):
