3a1536f63e1e732d3b38af0f76a32713c6497f11
love-adelar <love.adelar@gmail.com>
2020-01-29 23:20:22 +0900

    self, request를 self.request로 수정

--- accounts/views.py
+++ accounts/views.py
@@ -12,46 +12,48 @@ from django import forms
 
 from .forms import CreateUserForm
 
-import logging
-
 
 class AccountCreateView(generic.edit.CreateView):
     template_name = 'accounts/signup.html'
     form_class = CreateUserForm
     success_url = reverse_lazy('login')
-    
+
     # def post(self, request):
     #     redirect_url = request.GET.get('next', settings.LOGIN_REDIRECT_URL)
     #     signup_form = UserCreationForm(request.POST)
     #     if signup_form.is_valid():
     #         user = signup_form.save()
     #         auth_login(request, user)
-    #         return redirect(redirect_url) 
-    #     return render(request, 'accounts/signup.html', {'signup_form':signup_form})
+    #         return redirect(redirect_url)
+    #     return render(request, 'accounts/signup.html',
+    #  {'signup_form':signup_form})
 
     # def get(self, request):
     #     signup_form = UserCreationForm()
-    #     return render(request, 'accounts/signup.html', {'signup_form':signup_form})
+    #     return render(request, 'accounts/signup.html',
+    #  {'signup_form':signup_form})
 
 
 # class LoginCreateView(generic.edit.CreateView):
 #     success_url = '/'
-    
+
 #     def post(self, request):
 #         redirect_url = request.GET.get('next', settings.LOGIN_REDIRECT_URL)
 #         login_form = AuthenticationForm(request, request.POST)
 #         if login_form.is_valid():
 #             auth_login(request, login_form.get_user())
 #             return redirect(redirect_url)
-#         return render(request, 'accounts/login.html', {'login_form': login_form})
+#         return render(request, 'accounts/login.html',
+#  {'login_form': login_form})
 
 #     def get(self, request):
 #         login_form = AuthenticationForm()
-#         return render(request, 'accounts/login.html', {'login_form': login_form})
+#         return render(request, 'accounts/login.html',
+#  {'login_form': login_form})
 
 class UserLoginView(LoginView):
     template_name = 'accounts/login.html'
 
     def form_invalid(self, form):
-        messages.error(self, request, '로그인에 실패하였습니다.', extra_tags='danger')
+        messages.error(self.request, '로그인에 실패하였습니다.', extra_tags='danger')
         return super().form_invalid(form)
