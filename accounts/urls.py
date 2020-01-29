8079495998e5a9d830bbadb47964694ef230e570
love-adelar <love.adelar@gmail.com>
2020-01-29 23:53:40 +0900

    django에 내장된 LoginView로 로그인 기능 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,10 @@
-from mysite import settings
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 
-urlpatterns= [
+
+urlpatterns = [
     path('signup/', views.AccountCreateView.as_view(), name='signup'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('login/', views.UserLoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout')
 ]
