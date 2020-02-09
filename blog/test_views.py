47de8eaa2f45054828479af8bf44efe0fc586ea9
LYK <dalinaum@gmail.com>
2020-02-09 18:13:52 +0900

    client 객체 활용해서 인자를 넘겨받도록 수정

--- blog/test_views.py
+++ blog/test_views.py
@@ -1,7 +1,8 @@
+from django.urls import reverse
 from django.contrib.sessions.middleware import SessionMiddleware
 from django.contrib.auth.models import AnonymousUser, User
 from django.http import HttpRequest
-from django.test import TestCase, RequestFactory
+from django.test import Client, TestCase, RequestFactory
 from django.template.loader import render_to_string
 from factory import Sequence, SubFactory
 from factory.django import DjangoModelFactory
@@ -21,6 +22,16 @@ class UserFactory(DjangoModelFactory):
     password = ''
     is_active = True
     is_superuser = False
+
+    @classmethod
+    def _prepare(cls, create, **kwargs):
+        password = kwargs.pop('password', None)
+        user = super(UserFactory, cls)._prepare(create, **kwargs)
+        if password:
+            user.set_password(password)
+            if create:
+                user.save()
+        return user
 
 
 class PostFactory(DjangoModelFactory):
@@ -46,51 +57,72 @@ def add_middleware_to_response(request, middleware_class):
 class PostIndexViewTest(TestCase):
     # All of the teset cases should be accessible to this request factory
     def setUp(self):
-        self.factory = RequestFactory()
+        self.client = Client()
     
-    def test_index_view_requires_session_middleware(self):
-        """ 
-        Test that index view returns a 200 response and uses the correct template
-        """
-        request = self.factory.get('post_list')
-        request = add_middleware_to_request(request, SessionMiddleware)
-        response = IndexView.as_view()(request)
+    def test_get_with_author(self):
+        user = UserFactory.create(password='1234')
+        post_1 = PostFactory.create(author=user)
+        post_2 = PostFactory.create(author=user)
+        post_3 = PostFactory.create()
+
+        self.client.login(username=user.username, password='1234')
+        response = self.client.get(reverse('post_list'))
+
+        # rendered = render_to_string(
+        #     'post_list.html',
+        #     {'posts': [post_1, post_2]}
+        # )
         self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, 'No posts found')
+        # self.assertEqual(response.content.decode('utf-8'), rendered)
+
+    def test_get_some_dude(self):
+        user = UserFactory.create(password='1234')
+        PostFactory.create_batch(3)
+
+        self.client.login(username=user.username, password='1234')
+        response = self.client.get(reverse('post_list'))
+
+        # rendered = render_to_string('post_list.html', {posts: []})
+        self.assertEqual(response.status_code, 200)
+        # self.assertContains(response, 'No posts found')
+        # self.assertEqual(response.content.decode('utf-8'), rendered)
+
+    def test_get_with_anonymous(self):
+        response = self.client.get(reverse('post_list'))
+
+        self.assertEqual(response.status_code, 200)
+        # self.assertIn(reverse('/accounts/login', response['']))
 
 
 class PostDetailViewTest(TestCase):
     def setUp(self):
-        self.view = DetailView.as_view()
-        self.request = HttpRequest()
-        setattr(self.request, 'method', 'GET')
-        setattr(self.request, 'user', AnonymousUser())
+        self.client = Client()
 
     def test_get_with_anonymous(self):
         post = PostFactory.create()
-        response = self.view(self.request, post_id=post.uuid)
+        response = self.client.get(reverse('post_detail', args=(post.uuid,)))
+        
         self.assertEqual(response.status_code, 200)
-        # self.assertIn('/accounts/login/', response[])
+        # self.assertIn('/accounts/login/', response[''])
 
     def test_get_with_author(self):
-        user = UserFactory.create()
+        user = UserFactory.create(password='1234')
         post = PostFactory.create(author=user)
 
-        setattr(self.request, 'user', user)
-        response = self.view(self.request, post_id=post.uuid).render()
-        # rendered = render_to_string('post_detail.html', {'post':post}) # TODO: Add more request dict items
-        self.assertEqual(response.status_code, 200)
-        # self.assertEqual(response.content.decode('utf-8'), rendered)
+        self.client.login(username=user.username, password='1234')
+        response = self.client.get(reverse('post_detail', args=(post.uuid,)))
 
     def test_get_with_some_dude(self):
-        user = UserFactory.create()
+        user = UserFactory.create(password='1234')
         post = PostFactory.create()
 
-        setattr(self.request, 'user', user)
-        response = self.view(self.request, post_id = post.uuid).render()
+        self.client.login(username=user.username, password='1234')
+        response = self.client.get(reverse('post_detail', args=(post.uuid, )))
 
         # rendered = render_to_string(
         #     'post_detail.html',
         #     {'error': 'The book was not found or you do not have permission to access the post.'}
         #     )
         self.assertEqual(response.status_code, 200)
-        # self.assertEqual(respone.content.decode('utf-8'), rendered)
+        # self.assertEqual(response.content.decode('utf-8'), rendered)
